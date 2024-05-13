e4b7f5c51ba3144efa44e5d9238838875da2b3a6
Aryansh Mahato <aryansh.mahato@sourcefuse.com>
2024-05-13 11:57:35 +0530

    fix(todo-service-service): add DefaultUserModifyCrudRepository in todo repository
    GH-0

--- services/todo-service/src/repositories/todo.repository.ts
+++ services/todo-service/src/repositories/todo.repository.ts
@@ -1,16 +1,21 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {Getter, inject} from '@loopback/core';
+import {DefaultUserModifyCrudRepository, IAuthUserWithPermissions} from "@sourceloop/core"
 import {PgDataSource} from '../datasources';
 import {Todo, TodoRelations} from '../models';
+import {AuthenticationBindings} from "loopback4-authentication";
 
-export class TodoRepository extends DefaultCrudRepository<
+export class TodoRepository extends DefaultUserModifyCrudRepository<
   Todo,
   typeof Todo.prototype.id,
   TodoRelations
 > {
   constructor(
     @inject('datasources.pg') dataSource: PgDataSource,
+    @inject.getter(AuthenticationBindings.CURRENT_USER)
+    protected readonly getCurrentUser: Getter<
+        IAuthUserWithPermissions | undefined
+    >
   ) {
-    super(Todo, dataSource);
+    super(Todo, dataSource, getCurrentUser);
   }
 }
